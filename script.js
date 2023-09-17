60a207995ea9ee55305447f33cefd5902fd2a080
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-09-17 19:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+    
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > firstMax) {
+            secondMax = firstMax;
+            firstMax = arr[i];
+        } else if (arr[i] < firstMax && arr[i] > secondMax) {
+            secondMax = arr[i];
+        }
+    }
+    
+    if (secondMax === -Infinity) {
+        return -Infinity; // All elements are the same or there's only one element.
+    }
+    
+    return secondMax;
+}
